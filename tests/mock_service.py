18346723a6e2fc8afbc6e29fa7d9c385c4d994c0
Paul Glendenning <paul@marbles.ai>
2017-09-18 12:06:04 -0700

    Test working.

---
+++ tests/mock_service.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals, print_function
+import os
+import sys
+from optparse import OptionParser
+
+
+# Modify python path
+thisdir = os.path.dirname(os.path.abspath(__file__))
+srcdir = os.path.dirname(thisdir)
+print(srcdir)
+sys.path.insert(0, srcdir)
+
+
+from mservice import executor
+
+
+class MockExecutor(executor.ServiceExecutor):
+
+    def __init__(self, state):
+        super(MockExecutor, self).__init__(wakeup=5*60, state_or_logger=state)
+
+    def on_start(self, workdir):
+        pass
+
+    def on_term(self, graceful):
+        pass
+
+
+if __name__ == '__main__':
+    usage = 'Usage: %prog [options]'
+    parser = OptionParser(usage)
+    executor.init_parser_options(parser)
+
+    (options, args) = parser.parse_args()
+
+    svc_name = os.path.splitext(os.path.basename(__file__))[0]
+    stream_name = 'svc-' + svc_name
+    state = executor.process_parser_options(options, svc_name, stream_name)
+
+    svc = MockExecutor(state)
+    svc.run(thisdir)
--- tests/svc_test.py
+++ tests/svc_test.py
@@ -1,10 +1,34 @@
+from __future__ import unicode_literals, print_function
 import unittest
+import subprocess
+import os
+import signal
+import time
+import psutil
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+THISDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class SvcTest(unittest.TestCase):
+
+    def test_Run(self):
+        daemon = subprocess.Popen(['python', os.path.join(THISDIR, 'mock_service.py'),
+                                        '--log-level', 'debug'])
+        # Wait for a bit
+        time.sleep(10)
+        # Check if success
+        ps = psutil.Process(daemon.pid)
+        os.kill(daemon.pid, signal.SIGINT)
+        time.sleep(5)
+        killed = False
+        try:
+            ps = psutil.Process(daemon.pid)
+        except psutil.NoSuchProcess:
+            killed = True
+        self.assertTrue(killed)
 
 
 if __name__ == '__main__':
     unittest.main()
+
